76b5fe08e0b8271437662f51f967e805a62e2a97
Felix Waldbach <felix.waldbach@hpe.com>
2018-04-15 21:22:53 +0200

    Added file databaseutils.js with functions to query the db to get wine, add wine, update wine, delete wine. Same with suppliers and addresses. Added routes to app.js to access the query functions. Added test file test.jade to test some of the queries. Added jsonDesigns for response formats

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 var createError = require('http-errors');
 var express = require('express');
+var cors = require('cors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var app = express();
+
+var databaseutils = require('./databaseutils');
+
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -16,15 +21,112 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get("/", function (req, res) {
+    res.render('test');
+});
+
 app.get("/wine/search", function (req, res) {
     let query = req.query;
-    //db abfrage mit query;
+    console.log(query);
     let tableResponse = {
         "tableHeader": ["colName1", "colName2", "colNameX"],
         "tableBody": [["colData11", "colData12", "colData1X"],["colData21", "colData22", "colData2X" ],["colDataX1", "colDataX2", "colDataXX" ]]
     };
     res.send(tableResponse);
-})
+});
+
+app.post("/wine/add", async function (req, res) {
+    console.log("Posting");
+    console.log(req.body.name);
+    let result = await databaseutils.addWine(req.body.name, req.body.region, req.body.location, req.body.year, req.body.amount, req.body.basePrice, req.body.sellPrice, req.body.supplierID);
+    console.log(result);
+    res.send(result);
+});
+
+app.post("/wine/update", async function (req, res) {
+    let result = await databaseutils.updateWine(req.body.id, req.body.name, req.body.region, req.body.location, req.body.year, req.body.amount, req.body.basePrice, req.body.sellPrice, req.body.supplierID);
+    console.log(result);
+    res.send(result);
+});
+
+app.post("/wine/delete", async function (req, res) {
+    let result = await databaseutils.deleteWine(req.body.id);
+    console.log(result);
+    res.send(result);
+});
+
+app.get("/wine/get", async function (req, res) {
+    let result = await databaseutils.getWines();
+    console.log(result);
+    res.send(result);
+});
+
+app.get("/wine/getById", async function (req, res) {
+    let result = await databaseutils.getWineById(req.body.id);
+    console.log(result);
+    res.send(result);
+});
+
+app.post("/supplier/add", async function (req, res) {
+    let result = await databaseutils.addSupplier(req.body.name, req.body.region, req.body.year, req.body.basePrice, req.body.sellPrice, req.body.supplierID);
+    console.log(result);
+    res.send(result);
+});
+
+app.post("/supplier/update", async function (req, res) {
+    let result = await databaseutils.updateSupplier(req.body.id, req.body.name, req.body.region, req.body.year, req.body.basePrice, req.body.sellPrice, req.body.supplierID);
+    console.log(result);
+    res.send(result);
+});
+
+app.post("/supplier/delete", async function (req, res) {
+    let result = await databaseutils.deleteSupplier(req.body.id);
+    console.log(result);
+    res.send(result);
+});
+
+app.get("/supplier/get", async function (req, res) {
+    let result = await databaseutils.getSuppliers();
+    console.log(result);
+    res.send(result);
+});
+
+app.get("/supplier/getById", async function (req, res) {
+    let result = await databaseutils.getSupplierById(req.body.id);
+    console.log(result);
+    res.send(result);
+});
+
+app.post("/address/add", async function (req, res) {
+    let result = await databaseutils.addAddress(req.body.street, req.body.number, req.body.post, req.body.city, req.body.country);
+    console.log(result);
+    res.send(result);
+});
+
+app.post("/address/update", async function (req, res) {
+    let result = await databaseutils.updateAddress(req.body.id, req.body.street, req.body.number, req.body.post, req.body.city, req.body.country);
+    console.log(result);
+    res.send(result);
+});
+
+app.post("/address/delete", async function (req, res) {
+    let result = await databaseutils.deleteAddress(req.body.id);
+    console.log(result);
+    res.send(result);
+});
+
+app.get("/address/get", async function (req, res) {
+    let result = await databaseutils.getAddresses();
+    console.log(result);
+    res.send(result);
+});
+
+app.get("/address/getById", async function (req, res) {
+    let result = await databaseutils.getAddressById(req.body.id);
+    console.log(result);
+    res.send(result);
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ databaseutils.js
@@ -0,0 +1,255 @@
+var mysql = require('mysql');
+
+var db = mysql.createConnection({
+    host: "localhost",
+    user: "admin",
+    password: "admin",
+    database: "semsa_weinhandel"
+});
+
+
+const addWine = (name, region, location, year, amount, basePrice, sellPrice, supplierID) =>
+    new Promise((resolve, reject) => {
+        console.log("Connected!");
+        let sql = "INSERT INTO artikel (bezeichnung, herkunft, lagerort, jahrgang, menge, einkaufspreis, verkaufspreis, lieferant_id) " +
+            "VALUES ('" + name + "','" + region + "','" + location + "','" + year + "','" + amount + "','" + basePrice + "','" + sellPrice + "','" + supplierID + "')";
+        db.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("Wine successfully added");
+        });
+    });
+
+const updateWine = (id, name, region, location, year, amount, basePrice, sellPrice, supplierID) =>
+    new Promise((resolve, reject) => {
+        console.log("Connected!");
+        let sql = "UPDATE artikel SET bezeichnung = '" + name + "', herkunft = '" + region + "', lagerort = '" + location + "', jahrgang = '" + year + "', menge = '" + amount + "', einkaufspreis = '" + basePrice + "', " +
+            "verkaufspreis = '" + sellPrice + "', lieferant_id = '" + supplierID + "' WHERE id = " + id;
+        db.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("Wine successfully updated");
+            resolve(result);
+        });
+    });
+
+const deleteWine = (id) =>
+    new Promise((resolve, reject) => {
+        console.log("Connected!");
+        let sql = "DELETE FROM artikel WHERE id = " + id;
+        db.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("Wine successfully deleted");
+            resolve(result);
+        });
+    });
+
+const getWines = () =>
+    new Promise((resolve, reject) => {
+        console.log("Connected!");
+        let sql = "SELECT * FROM artikel";
+        db.query(sql, function (err, result) {
+            if (err) throw err;
+            let jsonResultArray = [];
+            for (let i = 0; i < result.length; i++) {
+                let jsonResult = {
+                    "name": result[i].bezeichnung,
+                    "region": result[i].herkunft,
+                    "location": result[i].lagerort,
+                    "year": result[i].jahrgang,
+                    "amount": result[i].menge,
+                    "deliveryDate": "some date",
+                    "basePrice": result[i].einkaufspreis,
+                    "sellPrice": result[i].verkaufspreis,
+                    "supplierID": result[i].lieferant_id
+                };
+                jsonResultArray.push(jsonResult);
+            }
+            resolve(jsonResultArray);
+        });
+    });
+
+const getWineById = (id) =>
+    new Promise((resolve, reject) => {
+        console.log("Connected!");
+        let sql = "SELECT * FROM artikel WHERE id = " + id;
+        db.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("Wine successfully returned");
+            let jsonResult = {
+                "name": result.bezeichnung,
+                "region": result.herkunft,
+                "location": result.lagerort,
+                "year": result.jahrgang,
+                "amount": result.menge,
+                "deliveryDate": "some date",
+                "basePrice": result.einkaufspreis,
+                "sellPrice": result.verkaufspreis,
+                "supplierID": result.lieferant_id
+            }
+            resolve(jsonResult);
+        });
+    });
+
+const addSupplier = (name, vorname, region, address_id) =>
+    new Promise((resolve, reject) => {
+        console.log("Connected!");
+        let sql = "INSERT INTO lieferant (name, vorname, region, adresse_id) " +
+            "VALUES ('" + name + "','" + vorname + "','" + region + "','" + address_id + "')";
+        db.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("Supplier successfully added");
+            resolve(result);
+        });
+    });
+
+const updateSupplier = (id, name, vorname, region, address_id) =>
+    new Promise((resolve, reject) => {
+        console.log("Connected!");
+        let sql = "UPDATE lieferant SET name = '" + name + "', vorname = '" + vorname + "', region = '" + region + "', adresse_id = '" + address_id + "' WHERE id = " + id;
+        db.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("Supplier successfully updated");
+            resolve(result);
+        });
+    });
+
+const deleteSupplier = (id) =>
+    new Promise((resolve, reject) => {
+        console.log("Connected!");
+        let sql = "DELETE FROM artikel WHERE id = " + id;
+        db.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("Supplier successfully deleted");
+            resolve(result);
+        });
+    });
+
+const getSuppliers = () =>
+    new Promise((resolve, reject) => {
+        console.log("Connected!");
+        let sql = "SELECT * FROM artikel";
+        db.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("Supplier successfully printed");
+            let jsonResultArray = [];
+            for (let i = 0; i < result.length; i++) {
+                let jsonResult = {
+                    "name": result[i].name,
+                    "vorname": result[i].vorname,
+                    "region": result[i].region,
+                    "address_id": result[i].adresse_id
+                };
+                jsonResultArray.push(jsonResult);
+            }
+            resolve(jsonResultArray);
+        });
+    });
+
+const getSupplierById = (id) =>
+    new Promise((resolve, reject) => {
+        console.log("Connected!");
+        let sql = "SELECT * FROM lieferant WHERE id = " + id;
+        db.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("Wine successfully returned");
+            let jsonResult = {
+                "name": result.name,
+                "firstName": result.vorname,
+                "region": result.region,
+                "address_id": result.adresse_id,
+            }
+            resolve(jsonResult);
+        });
+    });
+
+const addAddress = (street, number, post, city, country) =>
+    new Promise((resolve, reject) => {
+        console.log("Connected!");
+        let sql = "INSERT INTO adresse (land, strasse, plz, ort, hausnummer) " +
+            "VALUES ('" + country + "','" + street + "','" + post + "','" + city + "','" + number + "')";
+        db.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("Address successfully added");
+            resolve(result);
+        });
+    });
+
+const updateAddress = (id, street, number, post, city, country) =>
+    new Promise((resolve, reject) => {
+        console.log("Connected!");
+        let sql = "UPDATE adresse SET strasse = '" + street + "', hausnummer = '" + number + "', plz = '" + post + "', " +
+            "ort = '" + city + "', land = '" + country + "' WHERE id = " + id;
+        db.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("Address successfully updated");
+            resolve(result);
+        });
+    });
+
+const deleteAddress = (id) =>
+    new Promise((resolve, reject) => {
+        console.log("Connected!");
+        let sql = "DELETE FROM adresse WHERE id = " + id;
+        db.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("Address successfully deleted");
+            resolve(result);
+        });
+    });
+
+const getAddresses = () =>
+    new Promise((resolve, reject) => {
+        console.log("Connected!");
+        let sql = "SELECT * FROM adresse";
+        db.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("Adresses successfully printed");
+            let jsonResultArray = [];
+            for (let i = 0; i < result.length; i++) {
+                let jsonResult = {
+                    "street": result[i].strasse,
+                    "number": result[i].hausnummer,
+                    "post": result[i].plz,
+                    "city": result[i].ort,
+                    "country": result[i].land
+                };
+                jsonResultArray.push(jsonResult);
+            }
+            resolve(jsonResultArray);
+        });
+    });
+
+const getAddressById = (id) =>
+    new Promise((resolve, reject) => {
+        console.log("Connected!");
+        let sql = "SELECT * FROM adresse WHERE id = " + id;
+        db.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("Wine successfully returned");
+            let jsonResult = {
+                "street": result.strasse,
+                "number": result.hausnummer,
+                "post": result.plz,
+                "city": result.ort,
+                "country": result.land
+            }
+            resolve(jsonResult);
+        });
+    });
+
+module.exports = {
+    addWine,
+    updateWine,
+    deleteWine,
+    getWines,
+    getWineById,
+    addSupplier,
+    updateSupplier,
+    deleteSupplier,
+    getSuppliers,
+    getSupplierById,
+    addAddress,
+    updateAddress,
+    deleteAddress,
+    getAddresses,
+    getAddressById,
+};
