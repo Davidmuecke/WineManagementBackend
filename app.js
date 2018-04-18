817009ff08d51fc6b4115e60e367e56ef28219df
Felix Waldbach <felix.waldbach@hpe.com>
2018-04-18 09:09:02 +0200

    Updated json responses and returns from select statements.

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.get("/", function (req, res) {
 });
 
 app.get("/wine/search", function (req, res) {
-    let query = req.body.query;
+    let query = req.query.query;
     console.log(query);
     let tableResponse = {
         "tableHeader": ["id0", "colName1", "colName2", "colNameX"],
@@ -49,8 +49,8 @@ app.post("/wine/update", async function (req, res) {
     res.send(result);
 });
 
-app.post("/wine/delete", async function (req, res) {
-    let result = await databaseutils.deleteWine(req.body.id);
+app.get("/wine/delete", async function (req, res) {
+    let result = await databaseutils.deleteWine(req.query.id);
     console.log(result);
     res.send(result);
 });
@@ -66,15 +66,16 @@ app.get("/wine/get", async function (req, res) {
     for (let w in wineResult) {
         let supplier = await databaseutils.getSupplierById(wineResult[w].lieferant_id);
         console.log(supplier);
-        tableBody.push([wineResult[w].nummer, wineResult[w].name, wineResult[w].jahrgang, wineResult[w].bestand, supplier.firstName + " " + supplier.name,
+        tableBody.push([wineResult[w].nummer, wineResult[w].name, wineResult[w].jahrgang, wineResult[w].bestand, supplier.name,
                         wineResult[w].einkaufspreis, wineResult[w].verkaufspreis, wineResult[w].anbauort, wineResult[w].lagerort]);
     }
+
     result.tableBody = tableBody;
     res.send(result);
 });
 
 app.get("/wine/getById", async function (req, res) {
-    let result = await databaseutils.getWineById(req.body.id);
+    let result = await databaseutils.getWineById(req.query.id);
     console.log(result);
     res.send(result);
 });
@@ -98,13 +99,25 @@ app.post("/supplier/delete", async function (req, res) {
 });
 
 app.get("/supplier/get", async function (req, res) {
-    let result = await databaseutils.getSuppliers();
-    console.log(result);
+    let supplierResult = await databaseutils.getSuppliers();
+    let result = {
+        "tableHeader": ["Kundennummer", "Name", "Region", "Land", "Straße", "Ort", "PLZ"],
+        "tableBody": []
+    };
+    let tableBody = [];
+    for (let s in supplierResult) {
+        let address = await databaseutils.getAddressById(supplierResult[s].adresse_id);
+        console.log(address);
+        tableBody.push([supplierResult[s].kundennummer, supplierResult[s].name, supplierResult[s].region, address.land, address.strasse,
+            address.ort, address.plz]);
+    }
+
+    result.tableBody = tableBody;
     res.send(result);
 });
 
 app.get("/supplier/getById", async function (req, res) {
-    let result = await databaseutils.getSupplierById(req.body.id);
+    let result = await databaseutils.getSupplierById(req.query.id);
     console.log(result);
     res.send(result);
 });
@@ -134,7 +147,7 @@ app.get("/address/get", async function (req, res) {
 });
 
 app.get("/address/getById", async function (req, res) {
-    let result = await databaseutils.getAddressById(req.body.id);
+    let result = await databaseutils.getAddressById(req.query.id);
     console.log(result);
     res.send(result);
 });
--- databaseutils.js
+++ databaseutils.js
@@ -14,8 +14,10 @@ const addWine = (name, region, location, year, deliveryDate, amount, basePrice, 
         let sql = "INSERT INTO artikel (bezeichnung, herkunft, lagerort, jahrgang, lieferdatum, menge, einkaufspreis, verkaufspreis, lieferant_id) " +
             "VALUES ('" + name + "','" + region + "','" + location + "','" + year + "','" + deliveryDate + "','" + amount + "','" + basePrice + "','" + sellPrice + "','" + supplierID + "')";
         db.query(sql, function (err, result) {
-            if (err) throw err;
-            console.log("Wine successfully added");
+            if (err) {
+                throw err;
+                console.log(err);
+            } else console.log("Wine successfully added");
         });
     });
 
@@ -25,8 +27,10 @@ const updateWine = (id, name, region, location, year, deliveryDate, amount, base
         let sql = "UPDATE artikel SET bezeichnung = '" + name + "', herkunft = '" + region + "', lagerort = '" + location + "', jahrgang = '" + year + "', lieferdatum = '" + deliveryDate + "', menge = '" + amount + "', einkaufspreis = '" + basePrice + "', " +
             "verkaufspreis = '" + sellPrice + "', lieferant_id = '" + supplierID + "' WHERE id = " + id;
         db.query(sql, function (err, result) {
-            if (err) throw err;
-            console.log("Wine successfully updated");
+            if (err) {
+                throw err;
+                console.log(err);
+            } else console.log("Wine successfully updated");
             resolve(result);
         });
     });
@@ -36,8 +40,10 @@ const deleteWine = (id) =>
         console.log("Connected!");
         let sql = "DELETE FROM artikel WHERE id = " + id;
         db.query(sql, function (err, result) {
-            if (err) throw err;
-            console.log("Wine successfully deleted");
+            if (err) {
+                throw err;
+                console.log(err);
+            } else console.log("Wine successfully deleted");
             resolve(result);
         });
     });
@@ -47,7 +53,10 @@ const getWines = () =>
         console.log("Connected!");
         let sql = "SELECT * FROM artikel";
         db.query(sql, function (err, result) {
-            if (err) throw err;
+            if (err) {
+                throw err;
+                console.log(err);
+            }
             let jsonResultArray = [];
             for (let i = 0; i < result.length; i++) {
                 let jsonResult = {
@@ -73,19 +82,21 @@ const getWineById = (id) =>
         console.log("Connected!");
         let sql = "SELECT * FROM artikel WHERE id = " + id;
         db.query(sql, function (err, result) {
-            if (err) throw err;
-            console.log("Wine successfully returned");
+            if (err) {
+                throw err;
+                console.log(err);
+            } else console.log("Wine successfully returned");
             let jsonResult = {
-                "nummer": result.id,
-                "name": result.bezeichnung,
-                "region": result.herkunft,
-                "location": result.lagerort,
-                "year": result.jahrgang,
-                "deliveryDate": result.lieferdatum,
-                "amount": result.menge,
-                "basePrice": result.einkaufspreis,
-                "sellPrice": result.verkaufspreis,
-                "supplierID": result.lieferant_id
+                "nummer": result[0].id,
+                "name": result[0].bezeichnung,
+                "jahrgang": result[0].jahrgang,
+                "bestand": result[0].menge,
+                "lieferant_id": result[0].lieferant_id,
+                "einkaufspreis": result[0].einkaufspreis,
+                "verkaufspreis": result[0].verkaufspreis,
+                "anbauort": result[0].herkunft,
+                "lagerort": result[0].lagerort,
+                "lieferdatum": result[0].lieferdatum
             }
             resolve(jsonResult);
         });
@@ -96,7 +107,10 @@ const searchWine = (query) =>
         console.log("Connected!");
         let sql = "SELECT * FROM artikel WHERE jahrgang = '" + query + "' OR menge = '" + query + "' OR bezeichnung = '" + query + "' lagerort = '" + query + "' OR herkunft = '" + query + "'";
         db.query(sql, function (err, result) {
-            if (err) throw err;
+            if (err) {
+                throw err;
+                console.log(err);
+            }
             let jsonResultArray = [];
             for (let i = 0; i < result.length; i++) {
                 let jsonResult = {
@@ -122,8 +136,10 @@ const addSupplier = (name, vorname, region, address_id) =>
         let sql = "INSERT INTO lieferant (name, vorname, region, adresse_id) " +
             "VALUES ('" + name + "','" + vorname + "','" + region + "','" + address_id + "')";
         db.query(sql, function (err, result) {
-            if (err) throw err;
-            console.log("Supplier successfully added");
+            if (err) {
+                throw err;
+                console.log(err);
+            } else console.log("Supplier successfully added");
             resolve(result);
         });
     });
@@ -133,8 +149,10 @@ const updateSupplier = (id, name, vorname, region, address_id) =>
         console.log("Connected!");
         let sql = "UPDATE lieferant SET name = '" + name + "', vorname = '" + vorname + "', region = '" + region + "', adresse_id = '" + address_id + "' WHERE id = " + id;
         db.query(sql, function (err, result) {
-            if (err) throw err;
-            console.log("Supplier successfully updated");
+            if (err) {
+                throw err;
+                console.log(err);
+            } else console.log("Supplier successfully updated");
             resolve(result);
         });
     });
@@ -144,8 +162,10 @@ const deleteSupplier = (id) =>
         console.log("Connected!");
         let sql = "DELETE FROM artikel WHERE id = " + id;
         db.query(sql, function (err, result) {
-            if (err) throw err;
-            console.log("Supplier successfully deleted");
+            if (err) {
+                throw err;
+                console.log(err);
+            } else console.log("Supplier successfully deleted");
             resolve(result);
         });
     });
@@ -155,15 +175,18 @@ const getSuppliers = () =>
         console.log("Connected!");
         let sql = "SELECT * FROM lieferant";
         db.query(sql, function (err, result) {
-            if (err) throw err;
-            console.log("Supplier successfully printed");
+            if (err) {
+                throw err;
+                console.log(err);
+            } else console.log("Supplier successfully printed");
             let jsonResultArray = [];
             for (let i = 0; i < result.length; i++) {
                 let jsonResult = {
+                    "kundennummer": result[i].id,
                     "name": result[i].name,
                     "vorname": result[i].vorname,
                     "region": result[i].region,
-                    "address_id": result[i].adresse_id
+                    "addresse_id": result[i].adresse_id
                 };
                 jsonResultArray.push(jsonResult);
             }
@@ -176,13 +199,16 @@ const getSupplierById = (id) =>
         console.log("Connected!");
         let sql = "SELECT * FROM lieferant WHERE id = " + id;
         db.query(sql, function (err, result) {
-            if (err) throw err;
-            console.log("Supplier successfully returned");
+            if (err) {
+                throw err;
+                console.log(err);
+            } else console.log("Supplier successfully returned");
             let jsonResult = {
+                "kundennummer": result[0].id,
                 "name": result[0].name,
-                "firstName": result[0].vorname,
+                "vorname": result[0].vorname,
                 "region": result[0].region,
-                "address_id": result[0].adresse_id,
+                "addresse_id": result[0].adresse_id
             };
             resolve(jsonResult);
         });
@@ -193,14 +219,18 @@ const searchSupplier = (query) =>
         console.log("Connected!");
         let sql = "SELECT * FROM lieferant WHERE name = '" + query + "' OR vorname = '" + query + "' OR region = '" + query + "'";
         db.query(sql, function (err, result) {
-            if (err) throw err;
+            if (err) {
+                throw err;
+                console.log(err);
+            }
             let jsonResultArray = [];
-            for (let i = 0; i < result.length; i++) {
+            for (let r in result) {
                 let jsonResult = {
-                    "name": result[i].name,
-                    "firstName": result[i].vorname,
-                    "region": result[i].region,
-                    "address_id": result[i].adresse_id,
+                    "kundennummer": result[r].id,
+                    "name": result[r].name,
+                    "vorname": result[r].vorname,
+                    "region": result[r].region,
+                    "addresse_id": result[r].adresse_id
                 };
                 jsonResultArray.push(jsonResult);
             }
@@ -214,8 +244,10 @@ const addAddress = (street, post, city, country) =>
         let sql = "INSERT INTO adresse (land, strasse, plz, ort) " +
             "VALUES ('" + country + "','" + street + "','" + post + "','" + city + "')";
         db.query(sql, function (err, result) {
-            if (err) throw err;
-            console.log("Address successfully added");
+            if (err) {
+                throw err;
+                console.log(err);
+            } else console.log("Address successfully added");
             resolve(result);
         });
     });
@@ -226,8 +258,10 @@ const updateAddress = (id, street, post, city, country) =>
         let sql = "UPDATE adresse SET strasse = '" + street + "', plz = '" + post + "', " +
             "ort = '" + city + "', land = '" + country + "' WHERE id = " + id;
         db.query(sql, function (err, result) {
-            if (err) throw err;
-            console.log("Address successfully updated");
+            if (err) {
+                throw err;
+                console.log(err);
+            } else console.log("Address successfully updated");
             resolve(result);
         });
     });
@@ -237,8 +271,10 @@ const deleteAddress = (id) =>
         console.log("Connected!");
         let sql = "DELETE FROM adresse WHERE id = " + id;
         db.query(sql, function (err, result) {
-            if (err) throw err;
-            console.log("Address successfully deleted");
+            if (err) {
+                throw err;
+                console.log(err);
+            } else console.log("Address successfully deleted");
             resolve(result);
         });
     });
@@ -248,15 +284,17 @@ const getAddresses = () =>
         console.log("Connected!");
         let sql = "SELECT * FROM adresse";
         db.query(sql, function (err, result) {
-            if (err) throw err;
-            console.log("Adresses successfully printed");
+            if (err) {
+                throw err;
+                console.log(err);
+            } else console.log("Adresses successfully printed");
             let jsonResultArray = [];
             for (let i = 0; i < result.length; i++) {
                 let jsonResult = {
-                    "street": result[i].strasse,
-                    "post": result[i].plz,
-                    "city": result[i].ort,
-                    "country": result[i].land
+                    "strasse": result[i].strasse,
+                    "plz": result[i].plz,
+                    "ort": result[i].ort,
+                    "land": result[i].land
                 };
                 jsonResultArray.push(jsonResult);
             }
@@ -269,13 +307,15 @@ const getAddressById = (id) =>
         console.log("Connected!");
         let sql = "SELECT * FROM adresse WHERE id = " + id;
         db.query(sql, function (err, result) {
-            if (err) throw err;
-            console.log("Wine successfully returned");
+            if (err) {
+                throw err;
+                console.log(err);
+            } else console.log("Wine successfully returned");
             let jsonResult = {
-                "street": result.strasse,
-                "post": result.plz,
-                "city": result.ort,
-                "country": result.land
+                "strasse": result[0].strasse,
+                "plz": result[0].plz,
+                "ort": result[0].ort,
+                "land": result[0].land
             }
             resolve(jsonResult);
         });
